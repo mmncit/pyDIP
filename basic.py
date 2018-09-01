6173821ff1b180cf6475fb4a79876b93fccca704
Md Mamunur Rashid <mmncit@gmail.com>
2018-09-01 03:02:30 -0600

    Creating a numpy array from an image file

--- basic.py
+++ basic.py
@@ -1,7 +1,14 @@
-from scipy import misc
+
+# tutorial site: http://www.scipy-lectures.org/advanced/image_processing
+
+from scipy import misc # submodule dedicated to image processing (n-dimensional images)
 import matplotlib.pyplot as plt
 
-f = misc.face()
-misc.imsave('face.png', f)# uses the Image module (PIL)
-plt.imshow(f)
-plt.show()
+face = misc.face()
+misc.imsave('face.png', face)#Writing an array to a file
+#plt.imshow(face)
+#plt.show()
+face = misc.imread('face.png') #Creating a numpy array from an image file
+print(type(face))
+print(face.shape, face.dtype)
+
